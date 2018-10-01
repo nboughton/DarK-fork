eb3ef5662757f1b3fb4bc018e1e521b167b1d4fd
ssf <ssf@linux-brox.fritz.box>
2018-10-01 22:30:07 +0200

    2018.10.01.203007

--- .buildsuserpm.sh
+++ .buildsuserpm.sh
@@ -22,7 +22,7 @@ make PREFIX=${_basedir}/dark-icon-theme/usr SYSCONFDIR=${_basedir}/dark-icon-the
 cat <<\EOFALL> ${_basedir}/dark-icon-theme.spec
 Buildroot: BUILDROOT
 Name: dark-icon-theme
-Version: 2018.10.01.202915
+Version: 2018.10.01.203007
 Release: 1
 Summary: Minimalism at its finest.
 License: CC-BY-SA_V4
